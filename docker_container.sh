b48458008ee993e9028b4f4aad6ac67c3566295c
tirtahakimpambudhi <awebthp@gmail.com>
2024-03-27 21:12:15 +0700

    Update docker_container.sh

--- docker_container.sh
+++ docker_container.sh
@@ -12,7 +12,19 @@ docker container stop name_container or container_ID
 docker container rm name_container
 
 
-#Logging di Container
+# Logging di Container
 docker container logs name_container
 # Realtime Logging di Container
 docker container logs -f name_container
+
+
+# Container Exec untuk masuk ke Container dan Mengeksekusi kode program di dalam container
+docker container exec -i -t name_container /bin/bash
+-i # Untuk input tetap aktif
+-t # Untuk mode terminal akses
+
+# Container Port secara default hanya bisa di akses di dalam container yang bersifat isolation
+# Agar Bisa Mengakses Port dari Container dengan port forwarding
+docker container create --name name --publish or -p porthost:portcontainer image:tag
+
+
