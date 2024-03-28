4a99d1a5e2802df7014e6109b0c3cb02e835c5cc
tirtahakimpambudhi <awebthp@gmail.com>
2024-03-28 21:35:22 +0700

    Update File docker_volume.sh v.1.2

--- docker_volume.sh
+++ docker_volume.sh
@@ -12,8 +12,11 @@ docker container create --name name_container -p port_host:port_container --moun
 # Backup Volume
 1. docker container stop # berhentikan container terlebih dahulu agar tidak ada perubahan
 2. docker create --name backup_container --mount "type=volume,source=name_volume,destination=folder_container" --mount "type=bind,source=folder_host,destination=folder_container" image:tag # buat container baru dengan 2 mount volume yang ingin kita backup dan bind sistme host
+ run --rm #agar setelah berhenti langsung kehapus container nya
 # Jadi cara kerja sistem nya  volume yang kita backup itu destination nya di taruh folder container untuk mount pertaman
 # mount kedua bind mount destination nya di folder container bebas namanya dan sumber nya di taruh ke host agar ke sharing 
 3. tar cvf folder_containar_bind_dest/backup_name.tar.gz /folder_container_volume_det
 # masuk ke terminal dan masuk folder destination dari mount volume jadikan tar.gz dan di pindahkan ke destination mount bind agar ke sharing keluar
+
+# Restore Volume
 
