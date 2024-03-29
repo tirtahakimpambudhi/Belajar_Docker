d485ce34e9de9b5710f04872eab9e122d0a9c4c4
tirtahakimpambudhi <awebthp@gmail.com>
2024-03-29 14:06:45 +0700

    Remove all file .sh

--- docker_image.sh
+++
@@ -1,8 +0,0 @@
-# Untuk Melihat Semua Image
-docker image ls
-# Cara Menginstall Image dari Docker Registery Bersifat Append
-docker image pull name:tag
-# Cara Hapus Image 
-docker image rm name:tag
-
-
