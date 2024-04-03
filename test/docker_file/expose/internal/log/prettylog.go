c0985468f45330ba10d18d9a89a7139e4fb11325
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-03 10:34:46 +0700

    Update test/docker_file/expose/internal/prettylog.go

--- test/docker_file/expose/internal/log/prettylog.go
+++ test/docker_file/expose/internal/log/prettylog.go
@@ -1,1 +1,30 @@
 package log
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	reset = "\033[0m"
+
+	black        = 30
+	red          = 31
+	green        = 32
+	yellow       = 33
+	blue         = 34
+	magenta      = 35
+	cyan         = 36
+	lightGray    = 37
+	darkGray     = 90
+	lightRed     = 91
+	lightGreen   = 92
+	lightYellow  = 93
+	lightBlue    = 94
+	lightMagenta = 95
+	lightCyan    = 96
+	white        = 97
+)
+
+func colorize(colorCode int, v string) string {
+	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), v, reset)
+}
