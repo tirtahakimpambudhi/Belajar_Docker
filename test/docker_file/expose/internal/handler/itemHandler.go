87f84972167c25ad9e18453aafe35b6ff99e1166
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-03 11:23:23 +0700

    Create test/docker_file/expose/internal/memory/stores.go , test/docker_file/expose/pkg/json/json.go & Update test/docker_file/expose/internal/handler/itemHandler.go

--- test/docker_file/expose/internal/handler/itemHandler.go
+++ test/docker_file/expose/internal/handler/itemHandler.go
@@ -1,1 +1,26 @@
 package handler
+
+import (
+	"go_expose/internal/memory"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type generalResponse struct {
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+type ItemHandler struct {
+	*memory.Stores
+}
+
+func (i *ItemHandler) GetItemByID(w http.ResponseWriter, r *http.Request)  {
+	id := r.PathValue("id")
+	_, err := uuid.Parse(id)
+	if err != nil {
+		
+		return
+	}
+}
---
+++ test/docker_file/expose/internal/memory/stores.go
@@ -0,0 +1,31 @@
+package memory
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type Item struct {
+	Name  string    `json:"name"`
+	Qtt   int       `json:"quantity"`
+	Price *Currency `json:"price"`
+}
+
+type Currency struct {
+	Code   string  `json:"code"`
+	Amount float64 `json:"amount"`
+	Name   string  `json:"name"`
+	Symbol string  `json:"symbol"`
+}
+
+type Stores struct {
+	DB map[string]*Item
+	m sync.Mutex
+}
+
+func (s *Stores) Save(item *Item)  {
+	s.m.Lock()
+	defer s.m.Unlock()
+	s.DB[uuid.NewString()] = item
+}
