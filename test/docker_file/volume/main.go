d11551579a08ef182f7bf9c7acb3a0fbfcce36d9
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-07 23:22:12 +0000

    Apr 8, 2024, 6:22 AM

--- test/docker_file/volume/main.go
+++ test/docker_file/volume/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"go_volume/internal/handler"
+	log "go_volume/internal/logging"
+	"go_volume/internal/middleware"
+	"net/http"
+	"os"
 )
 
-func main()  {
-	fmt.Println("hello")
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8081"
+	}
+	mux := http.NewServeMux()
+	handl := handler.NewItemHandler()
+	mux.HandleFunc("GET /item/{id}",handl.GetItemByID)
+	mux.HandleFunc("GET /items",handl.GetAll)
+	mux.HandleFunc("POST /item",handl.CreateItems)
+	logging := middleware.NewLogging(log.SetupLogger())
+	loggingMiddleware := logging.LoggingMiddleware(mux)
+	recoverMiddleware := middleware.RecoverMiddleware(loggingMiddleware)
+	http.ListenAndServe(fmt.Sprintf(":%s",port), recoverMiddleware)
 }
