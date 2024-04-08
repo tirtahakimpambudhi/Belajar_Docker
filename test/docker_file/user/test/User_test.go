c6e4ca5b1a27b297c3b12f924f40c075f019ca4c
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-08 00:05:44 +0000

    Apr 8, 2024, 7:05 AM

--- test/docker_file/user/test/User_test.go
+++ test/docker_file/user/test/User_test.go
@@ -1,9 +1,15 @@
 package test
 
 import (
+	"os/exec"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestWhoami(t *testing.T)  {
-	
+	cmd := exec.Command("whoami")
+	user, err := cmd.Output()
+	require.NoError(t,err)
+	require.Equal(t,"tirtahakim\n",string(user))
 }
