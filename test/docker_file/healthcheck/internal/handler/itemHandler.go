f630e359617b899daad2d53286c1555e28032169
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-08 01:36:42 +0000

    Apr 8, 2024, 8:36 AM

--- test/docker_file/healthcheck/internal/handler/itemHandler.go
+++ test/docker_file/healthcheck/internal/handler/itemHandler.go
@@ -57,16 +57,26 @@ func (i *ItemHandler) CreateItems(w http.ResponseWriter , r *http.Request) {
 }
 
 func (i *ItemHandler) HealthItems(w http.ResponseWriter,r *http.Request)  {
-	
+	var wg *sync.WaitGroup
+	codes , err := i.seed(wg, 10)
+	for _, code := range codes {
+		if code == http.StatusInternalServerError {
+			helperJson.WriteJSON(w,code,&generalResponse{Message: "unhealthy", Data: map[string]any{
+				"code": code,
+				"error" : err.Error(),
+			}})
+			return
+		}
+	}
 }
 
 
-func (i *ItemHandler) seed(wg *sync.WaitGroup,total int) []int {
+func (i *ItemHandler) seed(wg *sync.WaitGroup,total int) ([]int,error) {
 	var actualyCodesStatus []int
 	item := &memory.Item{Name: "Test",Qtt: 10,Price: &memory.Currency{Code: "US",Amount: 40.0,Name: "Dollar",Symbol: "$"}}
 	payload , err := json.MarshalIndent(item,"", "    ")
 	if err != nil {
-		return []int{http.StatusInternalServerError}
+		return []int{http.StatusInternalServerError},err
 	}
 	for n := 0; n < total; n++ {
 		wg.Add(1)
@@ -78,5 +88,5 @@ func (i *ItemHandler) seed(wg *sync.WaitGroup,total int) []int {
 			defer wg.Done()
 		}()
 	}
-	return actualyCodesStatus
+	return actualyCodesStatus,nil
 }
